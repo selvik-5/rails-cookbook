c31b1664820b40c7a3a7191661505824ee94d8c4
vinayagaselvi kannappan <vinayagaselvi.kannappan@gmail.com>
2025-05-12 12:16:12 +0100

    rails new

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  has_many :recipe, through: :bookmarks
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,7 @@
 class Recipe < ApplicationRecord
+  has_many :bookmarks
+
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 end
